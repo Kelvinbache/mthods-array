d95ee25fc410601272880e5361ee40477f8770de
kelvin abche <kelvinabache12@gmail.com>
2023-09-19 16:48:56 -0400

    metodo map

---
+++ method_map/map.js
@@ -0,0 +1,11 @@
+const listUser = [
+  { user: "kelvin abache", age: 18, phone: 1231321321 },
+  { user: "santiago abache", age: 18, phone: 1231321321 },
+  { user: "kelly abache", age: 18, phone: 1231321321 },
+  { user: "manuel abache", age: 18, phone: 1231321321 },
+];
+
+const resultOFListUser = listUser.map(travelListUSer =);
+
+
+console.log(resultOFListUser)
