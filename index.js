ab77dc04e70e7012555e14b968ce9a8d79b6f355
kelvin abche <kelvinabache12@gmail.com>
2023-09-19 15:31:52 -0400

    clases anteriores

---
+++ index.js
@@ -0,0 +1,32 @@
+//metodos de arreglo funcion y por que usarlos
+
+// //concat
+// const arrayOne = [1,2,3];
+// const arrayTwo = [4,5,6];
+// const paste = arrayOne.concat(arrayTwo); /// concat es un metodo para pegar los datos a otro arreglo, pero los datos siempre seran de ultimo o primero
+// console.log(paste)
+
+//express operaitor
+// const arrayOne = [1, 2, 3];
+// const arrayTwo = [4, 5, 6];
+
+// const paste = [...arrayOne, ...arrayTwo]; //tambien es otra forma de concatenar los datos de un arreglo
+// console.log(paste);
+
+//join y undershit
+
+//join  const arrayOne = [1,2,3];
+// const arrayTwo = [4,5,6];
+// const paste = arrayOne.join(arrayTwo); // join es una forma de agregar datos a un arreglo, pero los datos siempre se pasar de ultimo
+// console.log(paste)
+
+
+const arrayOne = [1,2,3]
+const arrayTwo = [4,5,6]
+const paste = arrayTwo.unshift(arrayOne);
+console.log(paste)
+
+
+git 
+
+
